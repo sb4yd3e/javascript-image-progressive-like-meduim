89124f39e44625c0e92c5f9fede16948724e6789
Attapon Mongkon <attapon@Attapons-iMac.local>
2017-12-13 10:47:19 +0700

    init

---
+++ src/jip.js
@@ -0,0 +1,21 @@
+/*!
+https://codepen.io/jmperez/pen/yYjPER
+ */
+
+window.onload = function () {
+    var placeholder = document.querySelector('.placeholder'),
+        small = placeholder.querySelector('.img-small')
+    // 1: load small image and show it
+    var img = new Image();
+    img.src = small.src;
+    img.onload = function () {
+        small.classList.add('loaded');
+    };
+    // 2: load large image
+    var imgLarge = new Image();
+    imgLarge.src = placeholder.dataset.large;
+    imgLarge.onload = function () {
+        imgLarge.classList.add('loaded');
+    };
+    placeholder.appendChild(imgLarge);
+}
